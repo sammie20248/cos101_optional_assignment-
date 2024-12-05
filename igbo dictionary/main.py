b7259156c4c5114e537c9e7360d8b572e05fef39
sammie20248 <ifeanyichukwu.tochukwu@binghamuni.edu.ng>
2024-12-05 03:29:48 +0100

    assigning for projet

---
+++ igbo dictionary/main.py
@@ -0,0 +1,22 @@
+# Interactive Igbo Dictionary
+igbo_dictionary = {
+    "ụlọ": "house",
+    "anyị": "we",
+    "mmiri": "water",
+    "anyịkụ": "sun",
+    "ụlọ akwụkwọ": "school",
+    "mazi"  : "elder",
+    "obim" : "heart",
+    "chukwu" : "God",
+}
+
+
+
+
+
+
+
+
+
+
+
